bea44b22f327d2051165d5d4d2fb9a5acf1cde2e
felipeforte <64996043+felipeforte@users.noreply.github.com>
2023-05-17 21:51:48 -0300

    Create 1118.java

---
+++ java/1118.java
@@ -0,0 +1,74 @@
+import java.io.IOException;
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner entrada = new Scanner(System.in);
+        Aluno a = new Aluno();
+        
+        double nota = entrada.nextDouble();
+        while (nota < 0 || nota > 10) {
+            System.out.println("nota invalida");
+            nota = entrada.nextDouble();
+        }
+        a.setAv1(nota);
+        nota = entrada.nextDouble();
+        while (nota < 0 || nota > 10) {
+            System.out.println("nota invalida");
+            nota = entrada.nextDouble();
+        }
+        a.setAv2(nota);
+        System.out.println("media = " + String.format("%.2f", a.media()));
+
+        System.out.println("novo calculo (1-sim 2-nao)");
+        int opcao = entrada.nextInt();
+        while (opcao != 2) {
+            if (opcao == 1) {
+                a = new Aluno();
+                nota = entrada.nextDouble();
+                
+                while (nota < 0 || nota > 10) {
+                    System.out.println("nota invalida");
+                    nota = entrada.nextDouble();
+                }
+                a.setAv1(nota);
+                nota = entrada.nextDouble();
+                while (nota < 0 || nota > 10) {
+                    System.out.println("nota invalida");
+                    nota = entrada.nextDouble();
+                }
+                a.setAv2(nota);
+                System.out.println("media = " + String.format("%.2f", a.media()));
+                System.out.println("novo calculo (1-sim 2-nao)");
+                opcao = entrada.nextInt();
+            } else {
+                System.out.println("novo calculo (1-sim 2-nao)");
+                opcao = entrada.nextInt();
+            }
+        }
+        
+    }
+}
+
+class Aluno {
+    private double av1;
+    private double av2;
+    
+    public void setAv1(double av1) {
+        this.av1 = av1;
+    }
+    public void setAv2(double av2) {
+        this.av2 = av2;
+    }
+    
+    public double getAv1() {
+        return av1;
+    }
+    public double getAv2() {
+        return av2;
+    }
+    
+    public double media() {
+        return (av1+av2)/2;
+    }
+}
